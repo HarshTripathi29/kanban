0cacdd94a328740a8bec567c7487d6288cd4ebfe
Harsh Tripathi <106900613+HarshTripathi29@users.noreply.github.com>
2024-08-05 07:40:43 +0530

    update the middleware
    create boards was not working

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,24 +1,29 @@
-// middleware/auth.js
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const JWT_SECRET = 'your_jwt_secret_key'; // Keep this in an environment variable
 
 const authenticate = async (req, res, next) => {
-    const token = req.header('Authorization').replace('Bearer ', '');
-    if (!token) {
-        return res.status(401).json({ message: 'No token provided, authorization denied' });
+  try {
+    const authHeader = req.header('Authorization');
+    
+    if (!authHeader) {
+      return res.status(401).json({ message: 'Authorization header is missing' });
     }
 
-    try {
-        const decoded = jwt.verify(token, JWT_SECRET);
-        req.user = await User.findById(decoded.userId).select('-password');
-        if (!req.user) {
-            return res.status(401).json({ message: 'User not found' });
-        }
-        next();
-    } catch (error) {
-        res.status(401).json({ message: 'Token is not valid' });
+    const token = authHeader.replace('Bearer ', '');
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({ _id: decoded.id });
+
+    if (!user) {
+      throw new Error();
     }
+
+    console.log('Request Headers:', req.headers);
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Please authenticate' });
+  }
 };
 
 module.exports = authenticate;
